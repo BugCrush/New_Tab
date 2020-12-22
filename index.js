26b4366c1fc614f98568588542b3ed90d100475a
Aditya Khattar <khattaraditya2001@users.noreply.github.com>
2020-12-22 15:49:44 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-var express = require('express');
-var app = express();
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-app.set('view engine', 'ejs');
+const hostname = 'localhost';
+const port = 6969;
 
+const server = http.createServer((req, res) => {
+    console.log('Request for ' + req.url + ' by method ' + req.method);
 
-app.get('/', function(req, res) {
-    res.render('pages/index');
+    if (req.method == 'GET') {
+        var fileUrl;
+        if (req.url == '/') fileUrl = '/index.html';
+        else fileUrl = req.url;
+
+        var filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    filePath = path.resolve('./public/404.html');
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    fs.createReadStream(filePath).pipe(res);
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            });
+        }
+        else if (fileExt == '.css') {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/css');
+            fs.createReadStream(filePath).pipe(res);
+        }
+        else if (fileExt == '.js') {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/javascript');
+            fs.createReadStream(filePath).pipe(res);
+        }
+        else {
+            filePath = path.resolve('./public/404.html');
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            fs.createReadStream(filePath).pipe(res);
+        }
+    }
+    else {
+        filePath = path.resolve('./public/404.html');
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(filePath).pipe(res);
+    }
 });
 
 
-app.listen(0110);
-console.log('0110 is the magic port');
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
