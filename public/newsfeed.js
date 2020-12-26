22ce6f727bebe6f5a4011697f72822a831ade277
Aditya Khattar <khattaraditya2001@users.noreply.github.com>
2020-12-26 16:16:57 +0530

    Added newsfeed.js but needs work

---
+++ public/newsfeed.js
@@ -0,0 +1,8 @@
+function showFeed() {
+    var x = document.getElementById("newsfeedlink");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
